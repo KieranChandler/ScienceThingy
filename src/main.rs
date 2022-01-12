8e79671e1cb55561c4ce78365be0f26dab9f7bf0
Kieran Chandler <bwoodyouth_14@hotmail.co.uk>
2022-01-12 18:34:47 +0000

    Fixed mutable warning

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn input_handler() {
 }
 
 fn main() {
-    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
