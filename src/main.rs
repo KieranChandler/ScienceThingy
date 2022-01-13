3a9389a50af3c8cfdd0d5881f7138f5eac4502b4
KingAkhenaten <christianclaayyee@gmail.com>
2022-01-12 23:12:12 -0500

    I CAN HAZ TRIANGLEZ

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 extern crate glium;
 
 use glium::glutin;
-use glium::Surface;
 use glium::glutin::event::VirtualKeyCode;
 use glium::glutin::event::ElementState;
 use glium::glutin::event::MouseButton;
 use glium::backend::glutin::glutin::dpi::PhysicalPosition;
-
-pub mod sphere;
-pub mod errors;
+use glium::Surface;
+use glium::implement_vertex;
 
 fn input_handler() {
     println!("Input!");
@@ -24,6 +22,13 @@ fn left_click(cursor_pos: (f64, f64)) {
 }
 
 fn main() {
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
     let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
@@ -52,22 +57,26 @@ fn main() {
     
     let program = glium::Program::from_source(
         &display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
-    // code for drawing sphere
-    let sphere = sphere::SphereBuilder::new()
-        .translate(0.0, 0.5, 0.0)
-        .scale(2.0, 3.0, 4.0)
-        .build(&display);
     
-    event_loop.run(move |ev, _, control_flow| {
-        let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
-        target.finish().unwrap();
-        // target.draw(&sphere, &sphere, &program,
-        //            &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+    // code for drawing triangle
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [0.0, 0.5] };
+    let vertex3 = Vertex { position: [0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    
 
+    event_loop.run(move |ev, _, control_flow| {
+        let mut frame = display.draw(); 
+        //let (mut frame, uniforms) = common::begin_frame(&display);
+        frame.clear_color(0.0, 0.0, 1.0, 1.0);
+        frame.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                   &Default::default()).unwrap();
+        frame.finish().unwrap(); 
         let next_frame_time = std::time::Instant::now()
             + std::time::Duration::from_nanos(16_666_667);
+        
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         match ev {
